6fc6689b3aa7f9c6532609cdd30d41a9715e5272
youbaidjis <youba.etud@gmail.com>
2022-05-05 15:18:33 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+  document.querySelector('form').addEventListener('submit', function (e){
+       e.preventDefault();
+       const inputedText = document.getElementById("new-task-description").value;// copie the input of the User
+       const outputElement = document.createElement("li");                       // create a list element                      
+       outputElement.textContent = inputedText; 
+       document.querySelector('#tasks').appendChild(outputElement);              // output the result 
+
+      })
+
+      function deleteElement(){
+        document.querySelector('ul').addEventListener("click", function(e){
+          e.preventDefault();
+          e.target.remove()
+        })
+      }
+      deleteElement()// delete element by clicking on it
+
+
+
+  });
+ 
+  
+
